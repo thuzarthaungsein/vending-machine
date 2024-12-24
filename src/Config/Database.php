ddcb180e9ff542806f638de169e0c5fe6e0269bf
Thuzar <thuzar.ts92@gmail.com>
2024-12-24 19:32:10 +0700

    Env file added

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -2,34 +2,43 @@
 
 namespace App\Config;
 
+use Dotenv\Dotenv;
 use PDO;
 use PDOException;
 
+require_once __DIR__ . '/../../vendor/autoload.php';
+
 class Database
 {
-    private $host = 'localhost';
-    private $db_name = 'vending_machine';
-    private $username = 'root';
-    private $password = 'password';
-    private $conn;
+    private static $conn = null;
+
+    public function __construct()
+    {
+        $dotenv = Dotenv::createImmutable(__DIR__. '/../../');
+        $dotenv->load();
+        $this->connect();
+    }
 
     // this is a connection class to manage database connection
     // to be cleaned and structured, other CRUD operations for products, users and transactions will be created as separate classes in "/models"
 
     public function connect()
     {
-        $this->conn = null;
-        try {
-            $this->conn = new PDO(
-                "mysql:host={$this->host};dbname={$this->db_name}",
-                $this->username,
-                $this->password
-            );
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo "Connection error: " . $e->getMessage();
+        self::$conn = null;
+        if (self::$conn === null) {
+            try {
+                self::$conn = new PDO(
+                    "mysql:host=".$_ENV['DB_HOST'].";dbname=" .$_ENV['DB_NAME'],
+                    $_ENV['DB_USER'],
+                    $_ENV['DB_PASSWORD']
+                );
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo "Connection error: " . $e->getMessage();
+            }
         }
-        return $this->conn;
+
+        return self::$conn;
     }
 
 }
