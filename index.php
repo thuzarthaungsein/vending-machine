83de7610285a1f75ecf6a71b03d91317fee73a29
Thuzar <thuzar.ts92@gmail.com>
2024-12-27 11:20:24 +0700

    index file moved

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+session_start();
+
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
+
+$headers = getallheaders();
+$authHeader = $headers['Authorization'] ?? '';
+
+if (strpos($request, '/api') === 0) {
+    require_once __DIR__ . '/routes/api.php';
+} else {
+    require_once __DIR__ . '/routes/web.php';
+}
